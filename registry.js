2a969364a89905159c0412a5b546a2aa008974b8
Danny Hawkins <danny@quiqup.com>
2019-07-18 21:05:33 +0300

    Expose registry and encodeFunction

--- registry.js
+++ registry.js
@@ -1,39 +1,41 @@
-'use strict';
+'use strict'
 
-const url = require('url');
-const http = require('http');
-const https = require('https');
+const url = require('url')
+const http = require('http')
+const https = require('https')
 
-const SchemaCache = require('./lib/schema-cache');
-const decodeFunction = require('./lib/decode-function');
-const encodeFunction = require('./lib/encode-function');
+const SchemaCache = require('./lib/schema-cache')
+const decodeFunction = require('./lib/decode-function')
+const encodeFunction = require('./lib/encode-function')
 
 function schemas(registryUrl) {
-  const parsed = url.parse(registryUrl);
+  const parsed = url.parse(registryUrl)
   const registry = {
     cache: new SchemaCache(),
     protocol: parsed.protocol.startsWith('https') ? https : http,
     host: parsed.hostname,
     port: parsed.port,
-    path: parsed.path,
-  };
+    path: parsed.path
+  }
 
-  if(parsed.auth) {
-    registry.auth = parsed.auth;
+  if (parsed.auth) {
+    registry.auth = parsed.auth
   }
 
   const decode = decodeFunction(registry)
-  const encodeKey = encodeFunction.bySchema('key', registry);
-  const encodeMessage = encodeFunction.bySchema('value', registry);
-  const encodeById = encodeFunction.byId(registry);
+  const encodeKey = encodeFunction.bySchema('key', registry)
+  const encodeMessage = encodeFunction.bySchema('value', registry)
+  const encodeById = encodeFunction.byId(registry)
 
   return {
+    registry,
     decode,
     decodeMessage: decode,
     encodeById,
     encodeKey,
     encodeMessage,
-  };
-};
+    encodeFunction
+  }
+}
 
-module.exports = schemas;
+module.exports = schemas
